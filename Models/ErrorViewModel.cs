f3e6c095382849f78ddc480da5d7c05017dd6249
Hipolito, Christian John A. <jnistianhipolitov7@gmail.com>
2025-06-20 15:33:05 +0800

    Act2-Models

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_Dev_Act2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
